ce014c15a820d3c76c5b40e29da98afec77762ff
impressiveaustin <47372004+impressiveaustin@users.noreply.github.com>
2022-11-06 17:31:39 -0500

    Edited AutoTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -6,13 +6,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous
 public class AutoTest extends LinearOpMode {
 
+    //static final double COUNTS_PER_MOTOR_REV = 28 ;
+    //static final double DRIVE_GEAR_REDUCTION = 13.7;
+    //static final double WHEEL_DIAMETER_INCHES = 4;
+    static final double COUNTS_PER_INCH = 45.2079566;
     HardwareBIGBRAINBOTS robot = new HardwareBIGBRAINBOTS();
 
     @Override
     public void runOpMode() {
         robot.init(this.hardwareMap);
-
         waitForStart();
-        robot.drive(1.0, 100);
+        double distance = 10;
+        int counts = (int)(COUNTS_PER_INCH*distance);
+        robot.drive(0.50, counts);
     }
 }
