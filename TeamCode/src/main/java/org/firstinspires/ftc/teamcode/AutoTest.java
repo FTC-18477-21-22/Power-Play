444e73076ddd5c055312bb608bc0b4883bc597ef
impressiveaustin <47372004+impressiveaustin@users.noreply.github.com>
2022-11-20 16:05:11 -0500

    Added IMU to AutoTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -2,9 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Autonomous
 public class AutoTest extends LinearOpMode {
@@ -15,10 +22,33 @@ public class AutoTest extends LinearOpMode {
     static final double COUNTS_PER_INCH = 45.2079566;
     HardwareBIGBRAINBOTS robot = new HardwareBIGBRAINBOTS();
 
+    private BNO055IMU imu;
+    static final double TURN_SPEED = 0.75;
+    static final double P_TURN_COEFF_1 = 0.025;
+    static final double P_TURN_COEFF_2 = 0.0035;
+    static final double HEADING_THRESHOLD = 0.5;
+    //imu stuff end
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         robot.init(this.hardwareMap);
+
+        //initialize IMU
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        Orientation orientation = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        telemetry.addData("Third angle", "%.1f", orientation.thirdAngle);
+        telemetry.update();
+        //initialize IMU end
+
         waitForStart();
         double distance = 10;
         int counts = (int)(COUNTS_PER_INCH*distance);
